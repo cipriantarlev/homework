094e76c77bedb2feadbcd7f7cc606a96323cb82e
Ciprian <Ciprian Tarlev>
2019-03-31 23:27:04 +0300

    Terminal version 1.0

--- src/tema_16_03_2019/Terminal.java
+++ src/tema_16_03_2019/Terminal.java
@@ -16,25 +16,63 @@ public class Terminal {
 	private Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		new Terminal().help();
+		new Terminal().start();
 	}
 			
-	public static void start() {
-		
+	public void start() {
+		System.out.print("C:\\Temp ");
+		boolean mark = true;
+		while(mark) {
+			switch (scan.next()) {
+			case "cd":
+				changeDirectory();
+				break;
+			case "copy":
+				copyFiles();
+				break;
+			case "xcopy":
+				copyFolder();
+				break;
+			case "del":
+				deleteFile();
+				break;
+			case "rd":
+				removeDirectory();
+				break;
+			case "dir":
+				showDirectories();
+				break;
+			case "mkdir":
+				createDirectory();
+				break;
+			case "ren":
+				renameFile();
+				break;
+			case "rendir":
+				renameFolder();
+				break;
+			case "move":
+				move();
+				break;
+			case "help":
+				help();
+				break;
+			case "exit":
+				mark = false;
+				break;
+			default:
+				System.out.println("Please enter a valid command!");
+			}
+		}
 	}
 	
 	public void changeDirectory() { // "cd" command
-		Path path1 = Paths.get("C:\\Users\\Ciprian");
-		System.out.print(path1 + " ");
-		path1 = Paths.get(scan.nextLine().substring(3));
-		System.out.println(path1);
+		System.out.println(Paths.get(scan.next()));
 	}
 	
 	public void copyFiles() { // "copy" command
-		Path source = Paths.get(scan.nextLine());
-		Path target = Paths.get(scan.nextLine());
 		try {
-			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(Paths.get(scan.next()), Paths.get(scan.next()), StandardCopyOption.REPLACE_EXISTING);
 			System.out.println("The file was copied.");
 		} catch (IOException e) {
 			System.out.println("The file is not found!!!");
@@ -42,10 +80,8 @@ public class Terminal {
 	}
 	
 	public void copyFolder() { // "xcopy" command
-		Path source = Paths.get(scan.nextLine());
-		Path target = Paths.get(scan.nextLine());
 		try {
-			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(Paths.get(scan.next()), Paths.get(scan.next()), StandardCopyOption.REPLACE_EXISTING);
 			System.out.println("The folder was copied.");
 		} catch (IOException e) {
 			System.out.println("The folder is not found!!!");
@@ -54,8 +90,7 @@ public class Terminal {
 	
 	public void deleteFile() { // "del" command
 		try {
-			Path path = Paths.get(scan.nextLine());
-			Files.delete(path);
+			Files.delete(Paths.get(scan.next()));
 			System.out.println("The files was deleted");
 		} catch (IOException e) {
 			System.out.println("The file is not found!!!");
@@ -64,8 +99,7 @@ public class Terminal {
 	
 	public void removeDirectory() { // "RD" command
 		try {
-			Path path = Paths.get(scan.nextLine());
-			Files.delete(path);
+			Files.delete(Paths.get(scan.next()));
 			System.out.println("The files was deleted");
 		} catch (IOException e) {
 			System.out.println("The file is not found!!!");
@@ -73,9 +107,8 @@ public class Terminal {
 	}
 	
 	public void showDirectories() { // "dir" command
-		Path path = Paths.get(scan.nextLine());
 		try {
-			Files.walk(path)
+			Files.walk(Paths.get(scan.next()))
 				 .map(path1 -> path1.toFile().getName())
 				 .sorted()
 				 .forEach(file -> System.out.println(file));		
@@ -87,9 +120,8 @@ public class Terminal {
 	}
 	
 	public void createDirectory() { // "mkdir" command
-		Path path = Paths.get(scan.nextLine());
 			try {
-				Files.createDirectory(path);
+				Files.createDirectory(Paths.get(scan.next()));
 				System.out.println("The directory was created");
 			} catch (FileAlreadyExistsException e){
 				System.out.println("The directory already exists!!!");
@@ -100,9 +132,9 @@ public class Terminal {
 	
 	
 	public void renameFile() { // "ren" command
-		Path path = Paths.get(scan.nextLine());
+		Path source = Paths.get(scan.nextLine());
 		File target = Paths.get(scan.nextLine()).toFile();
-		boolean isrenamed = path.toFile().renameTo(target);
+		boolean isrenamed = source.toFile().renameTo(target);
 		if (isrenamed == true) {
 			System.out.println("The file was renamed");
 		}else {
@@ -111,9 +143,9 @@ public class Terminal {
 	}
 	
 	public void renameFolder() { // "rendir" command
-		Path path = Paths.get(scan.nextLine());
-		File target = Paths.get(scan.nextLine()).toFile();
-		boolean isrenamed = path.toFile().renameTo(target);
+		Path source = Paths.get(scan.next());
+		File target = Paths.get(scan.next()).toFile();
+		boolean isrenamed = source.toFile().renameTo(target);
 		if (isrenamed == true) {
 			System.out.println("The folder was renamed");
 		}else {
@@ -122,10 +154,8 @@ public class Terminal {
 	}
 	
 	public void move() { 
-		Path source = Paths.get(scan.nextLine());
-		Path target = Paths.get(scan.nextLine());
 		try {
-			Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+			Files.move(Paths.get(scan.next()), Paths.get(scan.next()), StandardCopyOption.REPLACE_EXISTING);
 			System.out.println("The file was moved.");
 		} catch (IOException e) {
 			System.out.println("The file cannot be moved!!!");
@@ -133,9 +163,8 @@ public class Terminal {
 	}
 	
 	public void help() {
-		Path help = Paths.get("help.txt");
 		try {
-			Files.readAllLines(help)
+			Files.readAllLines(Paths.get("help.txt"))
 				 .forEach(line -> System.out.println(line));
 		} catch (IOException e) {
 			System.out.println("The folder was no found!!!");
